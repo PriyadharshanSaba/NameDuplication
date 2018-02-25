fd6e9c9d20ff47d02ed5d1fbe439305d89f275d2
PriyadharshanSaba <priyadharshan.97@gmail.com>
2018-02-25 13:05:54 +0530

    Decision Tree (lname)

--- analytics.py
+++ analytics.py
@@ -8,15 +8,13 @@ def fetch_unique_records():
     fname = dset.iloc[:,3:4]
     lname = dset.iloc[:,:1]
     ufn = dset.fn.unique()
-    name_frame=pd.concat([fname,lname], axis=1) #concating the dataframe columns
+    name_frame=pd.concat([fname,lname], axis=1)                     #concating the dataframe columns
     j=0
     for i in ufn:
         if j==0:
             first_name_check = i
             fname_group=dset[dset['fn']==first_name_check]['ln']
-            #fname_group=dset[dset['fn']=='WILLIAM']['ln']
             lname_group=(fname_group.to_frame()).ln
-    #        print (fname_group)
             decision_tree_split(j,i.split(' '),lname_group)
             j+=1
         else:
@@ -26,44 +24,57 @@ def fetch_unique_records():
 def decision_tree_split(index,fname,lname):
     temp_list = list()
     if len(fname) == 1:
+        io=0
         for lname_split in lname:
-            if len(lname_split.split(' ')) >= 2:
-                #                if not lname_split in lnameleft :
+            io+=1
+            if len(lname_split.split(' ')) >= 2: # and io<=2:
                 if not existence_rules(lname_split):
                     lnameleft.append(list(lname_split.split()))
                 else:
-                    lnameright.append(lname_split)
+                    lnameright.append(list(lname_split.split()))
             else:
                 continue
 
 
 def existence_rules(xname):
-    #df['lname'] = df['First'].astype(str).str[0]
-    #print(df['ln'].astype(str).str[2])
-
     leng=len(xname.split())
-    if leng==1: #length of the Last name
+    if leng==1:                                                     #length of the Last name
         dfx = pd.DataFrame({'ln':lnameleft})
-        if xname in lnameleft:
+        if not presence(xname):
             return True
         elif xname[:1] in df['ln'].astype(str).str[2]:
             return True
         else:
             return False
-
     else:
-        #print(">>>>>>>>>>>>\t\t",xname)
         dfx = pd.DataFrame({'ln':lnameleft})
-        print(dfx)
-        for i in xname.split():
-            if i in lnameleft:
-                return True
-            elif i[:1] in dfx['ln'].astype(str).str[2]:
-                return True
-            else:
-                return False
+        if not presence(xname):
+            return False
+        else:
+            return True
+#        elif i[:1] in dfx['ln'].astype(str).str[2]:
+#            return True
+#        else:
+#            return False
+
 
 def dob():
+    return
+
+
+def presence(nam):
+    if len(lnameleft)==0:
+        return False
+    else:
+        for i in lnameleft:
+            for j in nam.split():
+                for k in i:
+                    if k == j:
+                        return True
+    return False
+
+
+
 
 
 dset = pd.read_csv('dset.csv')
@@ -73,6 +84,14 @@ lnameleft, lnameright = list(), list()
 fetch_unique_records()
 print("Left List:\n",lnameleft)
 print("\nRight List:\n",lnameright)
+
+
+
+
+
+
+
+
 
 
 
